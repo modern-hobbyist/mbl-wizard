9fc7e9fe79d929a50630d9336240d59efcfe1ef0
Charlie Steenhagen <csteen1005@gmail.com>
2022-10-27 17:15:22 -0500

    Moved some stuff around

--- src/renderer.ts
+++ src/renderer.ts
@@ -28,7 +28,7 @@
 
 import './index.css';
 // Add this to the end of the existing file
-import './renderer/app';
+import './app';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
 
