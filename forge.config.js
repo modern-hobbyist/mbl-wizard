cccf7c61dab82730254c6c05c3159aaa4919b1d6
Charlie Steenhagen <csteen1005@gmail.com>
2022-10-27 19:14:18 -0500

    Fixed webpack issue?

---
+++ forge.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "packagerConfig": {},
+    "makers": [
+        {
+            "name": "@electron-forge/maker-squirrel",
+            "config": {
+                "name": "MBL Wizard"
+            }
+        },
+        {
+            "name": "@electron-forge/maker-deb",
+            "config": {}
+        },
+        {
+            "name": "@electron-forge/maker-rpm",
+            "config": {}
+        },
+        {
+            "name": "@electron-forge/maker-dmg",
+            "config": {
+                "format": "ULFO"
+            },
+            "platforms": [
+                "darwin"
+            ]
+        }
+    ],
+    "plugins": [
+        {
+            "name": "@electron-forge/plugin-webpack",
+            "config": {
+                "mainConfig": "./webpack.main.config.js",
+                "renderer": {
+                    "config": "./webpack.renderer.config.js",
+                    "entryPoints": [
+                        {
+                            "html": "./src/index.html",
+                            "js": "./src/renderer.ts",
+                            "name": "main_window",
+                            "preload": {
+                                "js": "./src/preload.ts"
+                            }
+                        }
+                    ]
+                },
+                "port": 3001,
+                "loggerPort": 9001
+            }
+        }
+    ]
+}
